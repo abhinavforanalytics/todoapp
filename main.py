7535e5bbce48d3b9e33938888f2c538c1e8fc8b0
Abhinav Sinha <absinha@linkedin.com>
2023-06-25 01:44:37 +0530

    Added functions modules

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
-##Custom function
-def get_todos(filepath="Files/todo.txt"):
-    '''
-    Reading the file todos
-    :rtype: object
-    Todos in list format
-    '''
-    with open(filepath, 'r') as file :
-        todos_local = file.readlines()
-    return todos_local
-
-def write_todos(todos_arg,filepath="Files/todo.txt"): #non-default parameter should always be before default parameter
-    '''
-    :param filepath: file path where the todos are located
-    :param todos_arg: todos that need to be written back
-    :return: None
-    '''
-    with open(filepath, 'w') as file :
-        file.writelines(todos_arg) ##Since we're writing,
-        # it doesn't need to return anything, there's nothing to capture
-
+from functions import get_todos,write_todos
 
 while True:
     user_action = input("Type Add, Show, Edit, Complete or Exit: ")
