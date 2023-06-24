3927a280e9e7039a7b3bc6c92deabeca9050bb7f
Abhinav Sinha <absinha@linkedin.com>
2023-06-25 01:37:40 +0530

    Fixed 'Complete' and 'Add' function

--- Experiments/File/bonus12.py
+++ Experiments/File/bonus12.py
@@ -1,15 +1,20 @@
 feet_inches = input("Enter feet and inches: ")
 
 
-def convert(feet_inches) :
+def parse(feet_inches) :
     f, i = map(float, feet_inches.split(' '))
-    return f * 0.3048 + i * 0.0254
+    return {"Feet" : f, "Inches" : i}
 
 
-value = convert(feet_inches)
+def convert(feet, inches) :
+    return feet * 0.3048 + inches * 0.0254
+
+
+parsed = parse(feet_inches)
+value = convert(parsed['Feet'],parsed['Inches'])
 # print(f'{value:.2f} mts')
 
 if value < 1:
     print("Not allowed on the ride")
 else :
-    print(f"Enjoy the ride! since the height is {value:.2f}")
+    print(f"Enjoy the ride! since the height is {value:.2f}mts or {parsed['Feet']} feet and {parsed['Inches']} inches")
