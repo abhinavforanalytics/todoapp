c9e79be5fba41a7942fd8a6d27d18961703932b0
Abhinav Sinha <absinha@linkedin.com>
2023-06-25 00:51:43 +0530

    Fixed 'Complete' and 'Add' function

---
+++ Experiments/File/Experiment 14.py
@@ -0,0 +1,11 @@
+text = """
+Print this as it is
+the new line is recognized as a new line here
+"""
+text1 = "This is a new line \
+        where we print \
+        Everything in a new line \
+        but it only prints in one line"
+
+print(text)
+print(text1)
