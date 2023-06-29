8ced0eef6fcd8bbe93eb3dccc2d0993b7cb6d77d
Abhinav Sinha <absinha@linkedin.com>
2023-06-29 13:29:51 +0530

    Added todo button event

--- todo_gui.py
+++ todo_gui.py
@@ -2,7 +2,7 @@ import functions
 import PySimpleGUI as pyg
 
 label = pyg.Text("Enter a new todo: ")
-input_box = pyg.InputText(tooltip="Enter todo: ", font=('Calibri'), key='todo1')
+input_box = pyg.InputText(tooltip="Enter todo: ", font=('Calibri'), key='todo')
 add_button = pyg.Button("Add")
 
 window = pyg.Window('Welcome to ToDo pro!',
@@ -15,8 +15,12 @@ while True:
     match event:
         case "Add":
             todos = functions.get_todos()
-            new_todo = values
+            new_todo = values['todo']+"\n"
             todos.append(new_todo)
+            functions.write_todos(todos)
+        case pyg.WIN_CLOSED:
+            break
+
 
 
 window.close()
