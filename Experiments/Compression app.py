20fb44ed572292bfb829219ba935fbd1ccdf8283
Abhinav Sinha <absinha@linkedin.com>
2023-07-07 23:00:16 +0530

    Compression App updated

--- Experiments/Compression app.py
+++ Experiments/Compression app.py
@@ -1,15 +1,27 @@
-import PySimpleGUI as pyg
+import PySimpleGUI as PYg
+from zipcreator import make_archive
 
-label1 = pyg.Text("Select files to compress: ")
-input_wid1 = pyg.Input()
-button_wid1 = pyg.FolderBrowse("Choose")
+label1 = PYg.Text("Select files to compress: ")
+input_wid1 = PYg.Input()
+button_wid1 = PYg.FilesBrowse("Choose", key="files")
 
-label2 = pyg.Text("Select destination folder: ")
-input_wid2 = pyg.Input()
-button_wid2 = pyg.FolderBrowse("Choose")
+label2 = PYg.Text("Select destination folder: ")
+input_wid2 = PYg.Input()
+button_wid2 = PYg.FolderBrowse("Choose", key='folder')
 
-window = pyg.Window("Compression Pro",
+compress_button = PYg.Button("Compress")
+output_label = PYg.Text(key="output_message")
+
+window = PYg.Window("Compression Pro",
                     layout=[[label1, input_wid1, button_wid1],
-                                               [label2, input_wid2, button_wid2]])
-window.read()
+                            [label2, input_wid2, button_wid2], [compress_button, output_label]])
+
+while True:
+    event, values = window.read()
+    print("event: ", event, "values: ", values)
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+    make_archive(filepaths, folder)
+    window["output_message"].update(value="Compression Completed!")
+
 window.close()
